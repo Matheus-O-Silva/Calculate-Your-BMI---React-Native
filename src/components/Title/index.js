b77377a425e96807182922ddab65009b7ec74a52
Matheus <matheuso.silva95@gmail.com>
2021-11-22 11:40:44 -0300

    alter git repository

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Title() {
   return (
     <View >
-      <Text>Calculate Your BMI   </Text>
+      <Text>Calculate Your BMI</Text>
     </View>
   );
 }
